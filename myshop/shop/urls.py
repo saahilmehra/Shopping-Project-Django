b9adbc5771d73db0fbe6e7cceea47a23a7e67c74
saahilmehra <saahilmehra1998@gmail.com>
2020-03-20 17:09:23 +0530

    User can now see available products, filter products according to category and can see full product details.

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,4 +1,4 @@
-from djnago.urls import path
+from django.urls import path
 from . import views
 
 app_name='shop'
@@ -6,5 +6,5 @@ app_name='shop'
 urlpatterns=[
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('<int:id/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -6,7 +6,7 @@ def product_list(request, category_slug=None):
     category=None
     categories=Category.objects.all()
     products=Product.objects.filter(available=True)
-    if category.slug:
+    if category_slug:
         category=get_object_or_404(Category, slug=category_slug)
         products=products.filter(category=category)
     return render(request, 'shop/product/list.html', {'category': category, 'categories':categories, 'products':products})
