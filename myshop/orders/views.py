9ecf0bc336ff912071e71da8301cfe10d1349cb6
saahilmehra <saahilmehra1998@gmail.com>
2020-03-21 21:04:41 +0530

    Enabled online payments through credit cards.

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.urls import reverse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def order_create(request):
@@ -15,7 +17,11 @@ def order_create(request):
 
             #clear the Cart
             cart.clear()
-            return render(request, 'orders/order/created.html', {'order':order})
+
+            #set the order in the session
+            request.session['order_id']=order.id
+            #redirect for payment
+            return redirect(reverse('payment:process'))
     else:
         form=OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart':cart, 'form':form})
